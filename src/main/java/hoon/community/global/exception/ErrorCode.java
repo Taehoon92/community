699bbbb07f925f33c2bbef5ee27553de9a77ffcd
Taehoon <hoontae92@gmail.com>
2023-03-22 09:44:52 +1100

    요청헤더에 따라서 API 에러메세지 국제화 구현

--- src/main/java/hoon/community/global/exception/ErrorCode.java
+++ src/main/java/hoon/community/global/exception/ErrorCode.java
@@ -9,42 +9,41 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     /* 400 BAD_REQUEST */
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "400.badRequest"),
 
-    BIND_ERROR(HttpStatus.BAD_REQUEST, "바인딩 오류입니다."),
+    BIND_ERROR(HttpStatus.BAD_REQUEST, "400.bindError"),
 
-    SAME_AS_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "이전 비밀번호와 현재 비밀번호가 같습니다."),
+    SAME_AS_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "400.sameAsOldPassword"),
 
-    WRONG_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "현재 비밀번호가 일치하지 않습니다."),
+    WRONG_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "400.wrongOldPassword"),
 
-    MISSING_REQUEST_HEADER(HttpStatus.BAD_REQUEST, "요청 헤더가 누락되었습니다"),
+    MISSING_REQUEST_HEADER(HttpStatus.BAD_REQUEST, "400.missingRequestHeader"),
 
     /* 401 UNAUTHORIZED */
-    LOGIN_FAILURE(HttpStatus.UNAUTHORIZED, "로그인 정보가 잘못되었습니다."),
-    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    LOGIN_FAILURE(HttpStatus.UNAUTHORIZED, "401.loginFailure"),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "401.unauthorizedUser"),
 
     /* 403 FORBIDDEN */
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "액세스가 거부되었습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "403.accessDenied"),
 
     /* 404 NOT_FOUND */
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보를 찾을 수 없습니다."),
-    ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "권한 정보를 찾을 수 없습니다."),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글 정보를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "404.userNotFound"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "404.postNotFound"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "404.commentNotFound"),
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 정보를 찾을 수 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "404.notFound"),
 
-    FILE_UPLOAD_FAILURE(HttpStatus.NOT_FOUND, "파일 업로드에 실패하였습니다."),
+    FILE_UPLOAD_FAILURE(HttpStatus.NOT_FOUND, "404.fileUploadFailure"),
 
     /* 405 METHOD_NOT_ALLOWED */
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "405.methodNotAllowed"),
 
-    UNSUPPORTED_EXTENSION(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 확장자입니다."),
+    UNSUPPORTED_EXTENSION(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "415.unsupportedExtension"),
 
     /* 500 INTERNAL_SERVER_ERROR */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500.internalServerError"),
 
-    CANNOT_CONVERT_NESTED_STRUCTURE(HttpStatus.INTERNAL_SERVER_ERROR, "중첩 구조 변환에 실패하였습니다."),
+    CANNOT_CONVERT_NESTED_STRUCTURE(HttpStatus.INTERNAL_SERVER_ERROR, "500.cannotConvertNestedStructure"),
 
     ;
 
--- src/main/java/hoon/community/global/exception/GlobalExceptionHandler.java
+++ src/main/java/hoon/community/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package hoon.community.global.exception;
 
 import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -12,75 +15,81 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.BindException;
+import java.util.Locale;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @Autowired
+    MessageSource messageSource;
+
     @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e) {
+    protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e, Locale locale) {
         log.error("handleCustomException: {}", e.getErrorCode());
+        log.info("MessageSource Test = {}", messageSource.getMessage(e.getErrorCode().getMessage(), null, locale));
+        log.info("MessageSource Test = {}", messageSource.getMessage(e.getErrorCode().getMessage(), null, locale));
         return ResponseEntity
                 .status(e.getErrorCode().getStatus().value())
-                .body(new ErrorResponse(e.getErrorCode()));
+                .body(new ErrorResponse(e.getErrorCode(), messageSource, locale));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e, Locale locale) {
         log.error("handleHttpRequestMethodNotSupportedException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
+                .body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED, messageSource, locale));
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(final Exception e) {
+    protected ResponseEntity<ErrorResponse> handleException(final Exception e, Locale locale) {
         log.error("handleException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, messageSource, locale));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    protected ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e) {
+    protected ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e, Locale locale) {
         log.error("handleRuntimeException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.BAD_REQUEST.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.BAD_REQUEST));
+                .body(new ErrorResponse(ErrorCode.BAD_REQUEST, messageSource, locale));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    protected ResponseEntity<ErrorResponse> handleAccessDeniedException(final AccessDeniedException e) {
+    protected ResponseEntity<ErrorResponse> handleAccessDeniedException(final AccessDeniedException e, Locale locale) {
         log.error("handleAccessDeniedException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.ACCESS_DENIED.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.ACCESS_DENIED));
+                .body(new ErrorResponse(ErrorCode.ACCESS_DENIED, messageSource, locale));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException e) {
+    protected ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException e, Locale locale) {
         log.error("handleAccessDeniedException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.NOT_FOUND.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.NOT_FOUND));
+                .body(new ErrorResponse(ErrorCode.NOT_FOUND, messageSource, locale));
 
     }
 
     @ExceptionHandler(MissingRequestHeaderException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+    protected ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(final MissingRequestHeaderException e, Locale locale) {
         log.error("handleMissingRequestHeaderException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.MISSING_REQUEST_HEADER.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.MISSING_REQUEST_HEADER));
+                .body(new ErrorResponse(ErrorCode.MISSING_REQUEST_HEADER, messageSource, locale));
     }
 
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+    public ResponseEntity<ErrorResponse> handleBindException(BindException e, Locale locale) {
         log.error("handleBindException: {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.BIND_ERROR.getStatus().value())
-                .body(new ErrorResponse(ErrorCode.BIND_ERROR));
+                .body(new ErrorResponse(ErrorCode.BIND_ERROR, messageSource, locale));
     }
 }
