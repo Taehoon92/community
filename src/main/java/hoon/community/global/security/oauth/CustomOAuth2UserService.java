91a4b39bfdc17bec80e5ba06356f871757e33b4d
Taehoon <hoontae92@gmail.com>
2023-04-18 10:15:58 +1000

    OAuth2 로그인시 Provider 겹치고 이메일주소 중복될때 Exception 핸들링해서 에러메세지 발생시키려고 했는데, 실패

--- src/main/java/hoon/community/global/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/hoon/community/global/security/oauth/CustomOAuth2UserService.java
@@ -2,9 +2,8 @@ package hoon.community.global.security.oauth;
 
 import hoon.community.domain.member.entity.Member;
 import hoon.community.domain.member.repository.MemberRepository;
-import hoon.community.domain.member.service.MemberService;
 import hoon.community.domain.role.entity.RoleType;
-import hoon.community.global.security.CustomUserDetails;
+import hoon.community.controller.exception.OAuthProviderMissMatchException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,10 +15,11 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import springfox.documentation.service.OAuth;
 
-import javax.management.relation.Role;
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -47,32 +47,26 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         Optional<Member> foundMember = memberRepository.findByEmail(userInfo.getEmail());
         Member member;
         Set<RoleType> roleTypes = new HashSet<RoleType>();
-        if(foundMember.isPresent()) {
+        if (foundMember.isPresent()) {
             //가입된 경우
             member = foundMember.get();
 
-            if(member.getAuthProvider() == null) { // 기존 회원 중 OAuth 첫 로그인
-                log.info("기존회원 첫 OAuth 로그인");
+            if (member.getAuthProvider() == null) { // 기존 회원 중 OAuth 첫 로그인
                 member.updateModifiedDate();
                 roleTypes = member.getRoleTypes();
                 roleTypes.add(RoleType.ROLE_SOCIAL);
                 member.updateRoles(roleTypes);
                 member.updateAuthProvider(authProvider);
-            } else if(member.getAuthProvider() != authProvider){ // OAuth로그인 Provider가 기존 Provider랑 다를때
-                log.info("다른 Auth Provider로 접근");
-            } else {
-                log.info("기존회원 로그인");
+            } else if (member.getAuthProvider() != authProvider) { // OAuth로그인 Provider가 기존 Provider랑 다를때
+//                throw new OAuthProviderMissMatchException("Wrong Provider! Try again with " + member.getAuthProvider());
+                //Custom Exception 핸들링하는 방법 도저히 못찾음
+                throw new OAuth2AuthenticationException("Wrong Provider! Try again with " + member.getAuthProvider());
             }
 
-
-        } else {
-            log.info("USER INFO = {}", userInfo);
-
+        } else { // 첫 로그인
             roleTypes.add(RoleType.ROLE_USER);
             roleTypes.add(RoleType.ROLE_SOCIAL);
 
-
-            log.info("AUTH PROVIDER = {}", authProvider);
             member = Member.builder()
                     .email(userInfo.getEmail())
                     .username(userInfo.getUsername())
@@ -81,16 +75,8 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                     .authProvider(authProvider)
                     .build();
 
-
             memberRepository.save(member);
-
-
         }
-
-
-
-
-
 
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
@@ -98,81 +84,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 userNameAttributeName
         );
 
-
-//        OAuthAttributes oAuthAttributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-//
-//        log.info("Custom OAuth2User Service - OAuthAttributes : = {}",oAuthAttributes);
-
-
-        /*
-        Assert.notNull(userRequest, "userRequest cannot be null");
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
-
-        ClientRegistration.ProviderDetails.UserInfoEndpoint userInfoEndpoint = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint();
-
-        String userInfoUri = userInfoEndpoint.getUri();
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        String userNameAttributeName = userInfoEndpoint.getUserNameAttributeName();
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-        Member member = saveOrUpdate(attributes);
-
-
-        Map<String, Object> attributes = getAttributes(userRequest);
-
-        return process()
-
-         */
     }
 
-/*
-    private Member createMember(OAuth2UserInfo userInfo, AuthProvider authProvider) {
-        Set<RoleType> roleTypes = new HashSet<>();
-        roleTypes.add(RoleType.ROLE_SOCIAL);
-        roleTypes.add(RoleType.ROLE_USER);
-
-        String mainRole = Member.setMainRole(roleTypes);
-
-
-        Member member = Member.builder()
-                .email(userInfo.getEmail())
-                .roleTypes(roleTypes)
-                .mainRole(mainRole)
-                .build();
-    }
-
-
- */
-
-/*
-    @Transactional
-    public void saveOrUpdate(OAuth2UserInfo userInfo) {
-
-        log.info("USER INFO = {}", userInfo);
-        log.info("USER INFO - SUB = {}",userInfo.getId());
-        log.info("USER INFO - NAME = {}",userInfo.getUsername());
-        log.info("USER INFO - EMAIL = {}",userInfo.getEmail());
-
-        Optional<Member> foundMember = memberRepository.findByEmail(userInfo.getEmail());
-        Member member;
-        Set<RoleType> roleTypes = new HashSet<RoleType>();
-        log.info("Found Member = {}", foundMember);
-        if(foundMember.isPresent()) {
-            //가입된 경우
-            member = foundMember.get();
-            member.updateModifiedDate();
-            roleTypes = member.getRoleTypes();
-            log.info("ROLE TYPES  = {}", roleTypes);
-//            roleTypes.add(RoleType.ROLE_SOCIAL);
-//            member.updateRoles(roleTypes);
-
-            log.info("이미 가입");
-
-        } else {
-            //새로 가입하는 경우
-            log.info("새로 가입");
-        }
-
-    }
-*/
 }
